88597db458de8827850feaa613542cfa7b83d50a
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-09-14 11:55:18 -0300

    Add files via upload

---
+++ Module2/Main.java
@@ -0,0 +1,53 @@
+package ProfMain;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+
+		Aluno a1 = new Aluno();
+		a1.nome = "Elton";
+		a1.matricula = 001;
+		a1.cpf = "111.444.555-11";
+
+		Aluno a2 = new Aluno();
+		a2.nome = "Stella";
+		a2.matricula = 2;
+		a2.cpf = "222.222.222-22";
+
+		Disciplina d1 = new Disciplina();
+		d1.alunos.add(a1);
+		d1.alunos.add(a2);
+		d1.nome = "Programacao 1";
+
+		Disciplina d2 = new Disciplina();
+		d2.nome = "Programacao 2";
+		d2.alunos.add(a1);
+		d2.alunos.add(a2);
+
+		Turma t1 = new Turma();
+		t1.identificador = 001;
+		t1.descricao = "SPW - 001";
+
+		t1.disciplinas.add(d1);
+		t1.disciplinas.add(d2);
+
+
+		for(int i=0; i < t1.disciplinas.size() ; i=i+1) {
+			Disciplina dAux = t1.disciplinas.get(i);
+
+			System.out.println("Disciplina = " + dAux.nome);
+
+			for(int j=0 ; j < dAux.alunos.size(); j=j+1) {
+				System.out.println("Nome Aluno = " + dAux.alunos.get(j).nome);
+			}
+
+			System.out.println("-----------");
+		}
+
+
+
+
+	}
+}
