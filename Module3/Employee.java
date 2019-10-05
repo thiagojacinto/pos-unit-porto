292226e14cd21be120453375c6cb9fd5e3ad91b4
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-10-05 14:37:05 -0300

    Add files via upload

---
+++ Module3/Employee.java
@@ -0,0 +1,13 @@
+package activity01;
+
+public class Employee {
+	// ATTRIBUTES:
+	String name;
+	int CPF;
+	
+	// METHODS:
+	public void printDocument() {
+		// Used to print some document
+		System.out.println("Printing document ...");
+	}
+}
