8ce8a11e0c5002ed6607e8ec90fdfbea618a2803
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-10-05 16:00:09 -0300

    Add files via upload

---
+++ Module3/Calculus.java
@@ -0,0 +1,18 @@
+package activity05;
+
+public class Calculus {
+	int a,b,c;
+	Double d,e;
+	
+	public int sum(int a, int b,int c)  {
+		return a+b+c;
+	}
+	
+	public int sum(int b, int a) {
+		return a+b;
+	}
+	public Double sum(Double a, Double b) {
+		return a+b;
+	}
+	
+}
