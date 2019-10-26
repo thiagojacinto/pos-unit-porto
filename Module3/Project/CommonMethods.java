88616eeb78dc5fe9619684639d6fa9ec51045e82
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-10-26 16:31:29 -0300

    Add files via upload

---
+++ Module3/Project/CommonMethods.java
@@ -0,0 +1,15 @@
+package activity15.loansManagement;
+
+import java.util.ArrayList;
+
+public interface CommonMethods {
+	
+//	// ATTRIBUTES
+//	ArrayList<Book> bookList = new ArrayList<Book>();
+	
+	// Register
+	public void register();
+	
+	// Erase
+	public void erase();
+}
