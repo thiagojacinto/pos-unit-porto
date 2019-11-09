92244ed03bb889c360e9c6bd2a7505bc0f95ce34
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-11-09 16:56:42 -0300

    Add files via upload

---
+++ Module4/Knot.java
@@ -0,0 +1,22 @@
+package br.unit.module4.activity05;
+
+public class Knot {
+	private int info;
+	private Knot nextKnot;
+	
+	// GETTERS AND SETTERS
+	public int getInfo() {
+		return info;
+	}
+	public void setInfo(int info) {
+		this.info = info;
+	}
+	public Knot getNextKnot() {
+		return nextKnot;
+	}
+	public void setNextKnot(Knot nextKnot) {
+		this.nextKnot = nextKnot;
+	}
+	
+	
+}
