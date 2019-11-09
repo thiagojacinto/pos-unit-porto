f008786a5101788b1de51944f7ddfdbe37136c53
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-11-08 21:40:15 -0300

    Add files via upload

---
+++ Module4/Activity01/Test.java
@@ -0,0 +1,34 @@
+package br.unit.module4.activity01;
+
+public class Test {
+	public static void main(String[] args) {
+		Stack vector = new Stack();
+
+		vector.push(1);
+		System.out.println(vector);
+		vector.push(2);
+
+		for (int i = 0; i < 28; i++) {
+			vector.push(i+15);
+		}
+		System.out.println("Before 'pop': " + vector
+				+ "\n and Top value " + vector.showTop()
+				+ "\n and Top is " + vector.getTop()
+				);
+		
+		System.out.println("\nIs stack empty? -> " + vector.isEmpty()
+		+ "\n or is it full? -> " + vector.isFull()
+				);
+		for (int i = 0; i < 7; i++) {
+			vector.pop();
+		}
+		System.out.println("After 'pop':  " + vector
+				+ "\n and Top value " + vector.showTop()
+				+ "\n and Top = " + vector.getTop()
+				);
+		
+		System.out.println("\nIs stack empty? -> " + vector.isEmpty()
+							+ "\n or is it full? -> " + vector.isFull()
+				);
+	}
+}
