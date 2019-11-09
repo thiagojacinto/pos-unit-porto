0703e4a306e8aa15ddb72c6a1129b7191ea9b4fc
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-11-09 10:43:55 -0300

    Add files via upload

---
+++ Module4/Queue.java
@@ -0,0 +1,71 @@
+package br.unit.module4.activity02;
+
+public class Queue {
+
+	// ATTRIBUTE
+	private int head = 0;
+	private int[] queue;
+	private static final int QUEUE_SIZE = 20;
+
+	// CONSTRUCTOR
+	public Queue() {	// Empty constructor
+		// TODO Auto-generated constructor stub
+		queue = new int[QUEUE_SIZE];
+	}
+	
+	public Queue(int queueSize) {
+		queue = new int[queueSize];
+	}
+	
+	// METHODS
+	
+	// To add
+	public void insertInQueue(int value) {
+		if (!isFull()) {
+			queue[head] = value;
+			head++;
+		} else {
+			System.out.println("Queue is full.");
+		}
+	}
+	
+	// To remove
+	public void unQueue() {
+		if (!isEmpty()) {
+			for (int i = 1; i <= head; i++) {
+				queue[i-1] = queue[i];
+			}
+			// The reduce the head identifier
+			head--;
+		} else {
+			System.out.println();
+		}
+	}
+	
+	public boolean isFull() {
+		if (head == queue.length - 1) {
+			return true;	// It's full
+		}
+		return false;
+	}
+	
+	public boolean isEmpty() {
+		if (head == 0) {
+			return true;	// It's empty
+		}
+		return false;
+	}
+	
+	// Show all valid elements (NOT zero)
+	public void showQueue() {
+		for (int i = 0; i < head; i++) {
+			System.out.println("[" + i + "] = " + queue[i]);
+		}
+	}
+	
+	public void getQueue() {
+		for (int i = 0; i < queue.length; i++) {
+			System.out.println("[" + i + "] = " + queue[i]);
+		}
+	}
+}
---
+++ Module4/Test.java
@@ -0,0 +1,33 @@
+package br.unit.module4.activity02;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(" * * * * Queue implementation * * * * ");
+		
+		Queue q = new Queue();
+		// Insert items
+		q.insertInQueue(1);
+		q.insertInQueue(58);
+		q.insertInQueue(12);
+		q.insertInQueue(13);
+		q.insertInQueue(50);
+		
+		// Insert in for
+		for (int i = 72; i < 83; i++) {
+			q.insertInQueue(i);
+		}
+
+		// Show queue
+		System.out.println("\nQueue content: ");
+		q.showQueue();
+
+		// Then remove some items
+		for (int i = 0; i < 6; i++) {
+			q.unQueue();
+		}
+
+		// Show queue
+		System.out.println("\nQueue content: ");
+		q.showQueue();
+	}
+}
