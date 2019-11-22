76d8dc206b386260134a99ec577f914aa229863a
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-11-21 21:43:34 -0300

    Add files via upload

---
+++ Module4/BubbleSort.java
@@ -0,0 +1,51 @@
+package br.unit.module4.activity06;
+
+public class BubbleSort {
+	// ATTRIBUTES
+	private int[] vector;
+
+	// CONSTRUCTOR
+	public BubbleSort(int[] vector) {
+		// TODO Auto-generated constructor stub
+		this.vector = vector;
+	}
+
+	/*
+	 * Ordering
+	 */
+	public void sort() {
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 1; j < vector.length; j++) {
+
+				int v1 = vector[j-1];
+				int v2 = vector[j];
+				int aux;
+				if (v1 > v2) {
+					// Change values
+					aux = v1;
+					v1 = v2;
+					v2 = aux;
+					// then put them inside 'vector'
+					vector[j-1] = v1;
+					vector[j] = v2;
+				}
+			}
+		}
+	}
+
+	public String showVector() {
+		String vectorString = "[";
+		for (int i = 0; i < vector.length; i++) {
+			//			System.out.println(vector[i]);
+			vectorString += " " + vector[i];
+		}
+		vectorString += " ]";
+		return vectorString;
+	}
+
+	// Getters and Setters
+
+	public int[] getVector() {
+		return vector;
+	}
+}
