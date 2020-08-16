f5d8b30c1329620385d7f388393c39f7dee17acb
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-16 01:59:24 -0300

    [task] desafio08 bhaskara calculation

--- Bootcamp1/desafios-logica/desafio08/Desafio08.java
+++ Bootcamp1/desafios-logica/desafio08/Desafio08.java
@@ -1,5 +1,45 @@
 package desafio08;
 
 public class Desafio08 {
-
+	
+	/**
+	 * Calcula as raízes de uma equação de segundo grau a partir da fórmula de Bhaskara
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return raízes, {@code int[2]}
+	 */
+	private Double[] calcularBhaskara(Double a, Double b, Double c) {
+		
+		Double delta = Math.pow(b, 2d) -  4 * a * c;
+		
+		Double[] bhaskara = new Double[2];
+		
+		bhaskara[0] = (-b - Math.sqrt(delta))/(2 * a);
+		bhaskara[1] = (-b + Math.sqrt(delta))/(2 * a);
+		
+		return bhaskara;
+	}
+	
+	public void exibirRaizes(Double a, Double b, Double c) {
+		
+		Double[] raizes = calcularBhaskara(a, b, c);
+		
+		if (raizes[0].isNaN()  || raizes[1].isNaN()) {
+			
+			System.out.println("Indeterminadas");
+			
+		} else if (raizes[0].equals(raizes[1])) {
+			
+			System.out.println(
+					String.format("X1 = %.0f",  raizes[0])
+					);
+		} else {
+		System.out.println(
+				String.format("X1 = %.0f ;  X2 = %.0f", raizes[0], raizes[1])
+				);
+		}
+	}
+	
+	
 }
--- Bootcamp1/desafios-logica/desafio08/Principal.java
+++ Bootcamp1/desafios-logica/desafio08/Principal.java
@@ -3,7 +3,12 @@ package desafio08;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Desafio08 desafio08 = new Desafio08();
+		
+		desafio08.exibirRaizes(1d, -5d, 4d);
+		desafio08.exibirRaizes(1d, 2d, 1d);
+		desafio08.exibirRaizes(7d, 6d, 2d);
 
 	}
 
