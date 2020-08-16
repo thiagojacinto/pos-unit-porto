725d7633f1feaeecc4c7b087a733a510e24d505f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-16 01:58:16 -0300

    [task] desafio06 String manipulation

--- Bootcamp1/desafios-logica/desafio06/Desafio06.java
+++ Bootcamp1/desafios-logica/desafio06/Desafio06.java
@@ -1,5 +1,35 @@
 package desafio06;
 
 public class Desafio06 {
-
+	
+	/**
+	 * A partir de uma frase {@code String[]}, retorna a palavra de maior extensão em caracteres.
+	 * @param frase {@code String[]}
+	 * @return palavra de maior extensão {@code String}
+	 */
+	private String maiorPalavra(String frase) {
+		String[] particoes = frase.split(" ");
+		
+		String maiorPalavra = particoes[0];
+		
+		for (int index = 0; index < particoes.length; index++) {
+			
+			maiorPalavra = 
+					maiorPalavra.length() < particoes[index].length()
+					? particoes[index]
+					: maiorPalavra;
+		}
+		
+		return maiorPalavra;
+	}
+	
+	/**
+	 * Exibe no console a maior palavra de uma determinada frase.
+	 * @param frase {@code String}
+	 */
+	public void exibirMaiorPalavra(String frase) {
+		System.out.println(
+				String.format("Maior Palavra = %s", maiorPalavra(frase))
+				);
+	}
 }
--- Bootcamp1/desafios-logica/desafio06/Principal.java
+++ Bootcamp1/desafios-logica/desafio06/Principal.java
@@ -3,7 +3,11 @@ package desafio06;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Desafio06 desafio06 = new Desafio06();
+		
+		desafio06.exibirMaiorPalavra("Hoje é o desafio vai ser legal");
+		desafio06.exibirMaiorPalavra("Amanhã é quarta e eu vou a praia");
 
 	}
 
